1dd741844a45c3b8ee7745acad97f5c98f782df3
Drixler Angelo <drixler97@gmail.com>
2020-11-14 21:15:31 +0800

    Changed the style of the Auth page
    - Made it look more like the requirements.
    - Started to fix the look of the app in the mobile view.

--- www/resources/js/components/page/AuthPage.vue
+++ www/resources/js/components/page/AuthPage.vue
@@ -2,16 +2,29 @@
     <div style="height: 100%;">
         <div class="hero-body">
             <div class="columns">
-                <div class="column is-10-mobile is-offset-1-mobile is-4-desktop is-offset-4-desktop">
+                <div class="column is-10-mobile is-offset-1-mobile">
                     <div class="title">Digital Bookmark</div>
-                    <input class="input" type="text" placeholder="Username">
-                    <input class="input" type="password" placeholder="Password">
 
-                    <button style="display: block; margin-left: auto; margin-right: auto; width: 40%;" class="button is-danger is-rounded">Login</button>
+                    <div class="field">
+                        <div class="control is-loading">
+                            <input class="input is-rounded" style="margin-top: 3em;" type="text" placeholder="Username">
+                        </div>
+                    </div>
 
-                    <div class="buttons has-addons">
-                        <button style="width: 50%" class="button">Signup</button>
-                        <button style="width: 50%" class="button">Login</button>
+
+                    <input class="input is-rounded" style="margin-top: 1em;" type="password" placeholder="Password">
+
+                    <button style="display: block; margin-left: auto; margin-right: auto; width: 25vw; margin-top: 1em; background-color: #BF2C1F;" class="button is-danger is-rounded">
+                        {{ currentMode }}
+                    </button>
+
+                    <div class="columns" style="margin-top: 1em;">
+                        <div class="signup-button column" @click="test('signup')" :style="signupStyle">
+                            SIGNUP
+                        </div>
+                        <div class="login-button column" @click="test('login')" :style="loginStyle">
+                            LOGIN
+                        </div>
                     </div>
                 </div>
             </div>
@@ -35,6 +48,61 @@
             'password-field' : PasswordField,
             'user-form'      : UserForm,
             'username-field' : UsernameField
+        },
+
+        data() {
+            return {
+                loginStyle  : {},
+                signupStyle : {},
+                currentMode : 'LOGIN',
+
+                //TODO: Pass this to CSS later
+                actionStyles : {
+                    login : {
+                        inactive: {
+                            'background': '#BF2C1F',
+                            'border-top-right-radius': 0,
+                            'margin-bottom': '2px',
+                            'margin-right': '2px',
+                            'color': '#F2F2F2'
+                        },
+                        active: {
+                            'background': '#FFF',
+                            'color': '#BF2C1F'
+                        }
+                    },
+                    signup : {
+                        inactive: {
+                            'background': '#BF2C1F',
+                            'border-top-left-radius': 0,
+                            'margin-bottom': '2px',
+                            'margin-left': '2px',
+                            'color': '#F2F2F2'
+                        },
+                        active: {
+                            'background': '#FFF',
+                            'color': '#BF2C1F'
+                        }
+                    }
+                }
+            };
+        },
+
+        methods : {
+            test(mode) {
+                this[mode + 'Style'] = this.actionStyles[mode].active;
+                this.currentMode = mode.toUpperCase();
+
+                if (mode === 'login') {
+                    this.signupStyle = this.actionStyles['signup'].inactive;
+                } else {
+                    this.loginStyle = this.actionStyles['login'].inactive;
+                }
+            }
+        },
+
+        created() {
+            this.test('login');
         }
     }
 </script>
@@ -47,6 +115,12 @@
 
     .column {
         background: white;
+        margin-top: 4em;
+        border-radius: 15px;
+    }
+
+    .columns {
+        display: flex;
     }
 
     .title {
@@ -58,16 +132,31 @@
         text-align: center;
     }
 
+    .input {
+        /* width: 25vw; */
+        margin-left: auto;
+        display: block;
+        margin-right: auto;
+    }
+
     @media screen and (min-width: 601px) {
         .title {
-            margin-top: 45px;
-            font-size: 48px;
+            margin-top: 0.5em;
+            font-size: 2em;
         }
 
-        .column {
-            margin-top: 4em;
-            box-shadow: 0 0 0 2em white;
-            border-radius: 15px;
+        .signup-button {
+            border-top-left-radius: 0;
+            border-bottom-right-radius: 0;
+            font-size: 1.1em;
+            text-align: center;
+        }
+
+        .login-button {
+            border-bottom-left-radius: 0;
+            border-top-right-radius: 0;
+            font-size: 1.1em;
+            text-align: center;
         }
     }
 
